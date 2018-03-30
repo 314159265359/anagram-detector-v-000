d7f9c9b6a4f7d31d2a5fbd8cfc6dc6260f177636
Karen G <kgraves0701@gmail.com>
2018-03-30 19:01:16 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,17 +6,17 @@ class Anagram
  end
 
  def match(list)
-  #  anagrams  =  @word.chars.to_a.permutation.map(&:join).uniq.each {|anag|}
-  #  list.select{|anagram| anagrams.include?(anagram)}
-  result = []
-  matcher = @word.split("").sort
-  list.each do |anagram|
-    anag = anagram.split("").sort
-    if anag == matcher
-      result << anagram
-    end
-  end
-  result
+   anagrams  =  @word.chars.to_a.permutation.map(&:join).uniq.each {|anag|}
+   list.select{|anagram| anagrams.include?(anagram)}
+  # result = []
+  # matcher = @word.split("").sort
+  # list.each do |anagram|
+  #   anag = anagram.split("").sort
+  #   if anag == matcher
+  #     result << anagram
+  #   end
+  # end
+  # result
  end
 
 end
