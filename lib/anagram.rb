9615e5e50469f94080328e87015c4beb9c8b3332
Karen G <kgraves0701@gmail.com>
2018-03-30 18:53:12 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,9 +8,6 @@ class Anagram
  def match(list)
    anagrams  =  @word.chars.to_a.permutation.map(&:join).uniq.each {|anag|}
    list.select{|anagram| anagrams.include?(anagram)}
-  #  @word.chars.to_a.permutation.map(&:join).uniq.each do |anag|
-  #  puts anag
-   #end
  end
 
 end
