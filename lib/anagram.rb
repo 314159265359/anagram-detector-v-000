55b9f66a0696309f7170779b21d0df6a4bb56fc8
Karen G <kgraves0701@gmail.com>
2018-03-30 18:42:23 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,13 @@
 class Anagram
- attr_accessor :word  
+ attr_accessor :word, :list   
 
  def initialize(word)
    @word = word
  end
- 
+
+ def match(list)
+   list.detect{|anagram| word == anagram}
+ end
 
 end
 
